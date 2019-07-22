57bc5ba85f6f61effa69892ce29fc33f45af0e86
Denis Makogon <denys.makogon@oracle.com>
2019-07-22 18:11:31 +0300

    java version auto-detect

--- apply_binaries.sh
+++ apply_binaries.sh
@@ -2,18 +2,18 @@
 
 set -xe
 
-java_version=${1:-"java-13"}
+java_version=$(javap -version | tr "." " " | awk '{print $1 }')
 lib_path="lib64"
 
-if [[ ${java_version} == "java-11" ]];
+if [[ ${java_version} == "11" ]];
 then
     lib_path="lib"
 fi
 
 mkdir -p /usr/local/include/opencv4
-curl -L https://github.com/denismakogon/java-opencv/raw/master/release/${java_version}/include_opencv4.tar.gz  | \
+curl -L https://github.com/denismakogon/java-opencv/raw/master/release/java-${java_version}/include_opencv4.tar.gz  | \
     tar xvz -C /
 
 mkdir -p /usr/local/${lib_path}
-curl -L https://github.com/denismakogon/java-opencv/raw/master/release/${java_version}/${lib_path}.tar.gz | \
+curl -L https://github.com/denismakogon/java-opencv/raw/master/release/java-${java_version}/${lib_path}.tar.gz | \
     tar xvz -C /
