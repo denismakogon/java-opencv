71d425f55b13267f434e5d7987d7e90eb50df8b9
Denis Makogon <denys.makogon@oracle.com>
2019-07-22 18:00:25 +0300

    cleanup builds

--- apply_binaries.sh
+++ apply_binaries.sh
@@ -10,11 +10,10 @@ then
     lib_path="lib"
 fi
 
-curl -L https://raw.githubusercontent.com/denismakogon/java-opencv/master/release/${java_version}/include_opencv4.tar.gz | \
-    tar xvz -C /usr/local/include/opencv4
+mkdir -p /usr/local/include/opencv4
+curl -L https://github.com/denismakogon/java-opencv/raw/master/release/${java_version}/include_opencv4.tar.gz  | \
+    tar xvz -C /
 
-curl -L https://raw.githubusercontent.com/denismakogon/java-opencv/master/release/${java_version}/${lib_path}.tar.gz | \
-    tar xvz -C /usr/local/${lib_path}
-
-curl -L https://raw.githubusercontent.com/denismakogon/java-opencv/master/release/${java_version}/include_opencv4.tar.gz | \
-    tar xvz -C /usr/local/${lib_path}/pkgconfig/opencv4.pc
+mkdir -p /usr/local/${lib_path}
+curl -L https://github.com/denismakogon/java-opencv/raw/master/release/${java_version}/${lib_path}.tar.gz | \
+    tar xvz -C /
--- release/package_shared.sh
+++ release/package_shared.sh
@@ -6,7 +6,4 @@ rm -fr /root/opencv_shared/${JAVAVERSION}
 mkdir /root/opencv_shared/${JAVAVERSION}/
 
 tar -czvf /root/opencv_shared/${JAVAVERSION}/${OPENCVFOLDER}.tar.gz /usr/local/${OPENCVFOLDER}
-
-tar -czvf /root/opencv_shared/${JAVAVERSION}/opencv4.pc.tar.gz /usr/local/${OPENCVFOLDER}/pkgconfig/opencv4.pc
-
 tar -czvf /root/opencv_shared/${JAVAVERSION}/include_opencv4.tar.gz /usr/local/include/opencv4
